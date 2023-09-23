155f6c349e3e79fba103548c0c08432cf8bb2308
Fabian leon <cfabianleon@outlook.com>
2023-09-23 03:40:48 -0500

    ci:triggers for name and language

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 
 const nameEnv = process.env.USERNAME; 
+const language = process.env.LANGUAGE; 
 
-if (nameEnv) {
-  console.log(`Hello, World! ${nameEnv}`);
+if (nameEnv && language) {
+  console.log(`Hello, World! ${nameEnv} and my favorite langauge is $  ${language} `);
 } else {
   console.error('ENV_NAME is not set.');
 }
